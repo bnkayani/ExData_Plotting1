f67f3ea762eba5ca474dd4f85a548d10a37d36d7
bnkayani <kayani.bushra@gmail.com>
2015-05-11 02:14:17 +0300

    Course Project 1 for Exploratory Data Analysis
    May 11 Bushra Noor

---
+++ plot2.R
@@ -0,0 +1,14 @@
+# Generating Plot 2 for exploratory data analysis Course Project 1
+
+# Reading and subsetting data.
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plotting Data and saving plot in Png file
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off() 
